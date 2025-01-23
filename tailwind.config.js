978d3120b62ad510815583dc25a45451c4621fd7
Hilmi-Vaskrona <hilmi08zr@gmail.com>
2025-01-23 14:21:37 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "headers" : "url('../img/fragrance-1600-x-900-wallpaper-uraca1rpl5xsaiuh.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
