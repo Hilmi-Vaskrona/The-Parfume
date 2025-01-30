d628681cc29c1211f8cc06fc9b7964fa3848add3
Hilmi-Vaskrona <hilmi08zr@gmail.com>
2025-01-30 11:34:23 +0700

    penambahan responsif

---
+++ main.js
@@ -0,0 +1,36 @@
+var xstatus = true;
+var nb = false;
+
+const tombol = document.getElementById('tombol');
+const menu = document.getElementById('menu');
+const navbar = document.getElementById('navbar');
+
+tombol.addEventListener('click', () => {
+    if (xstatus) {
+        navbar.classList.add("bg-yellow-600");
+        menu.classList.remove("hidden");
+        menu.classList.add("flex");
+        xstatus = false;
+    } else {
+        if (nb == false || window.scrollY < 51) {
+            navbar.classList.remove("bg-yellow-600");
+        }
+        menu.classList.remove("flex");
+        menu.classList.add("hidden");
+        xstatus = true;
+    }
+});
+
+document.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        console.log("scroll");
+
+        navbar.classList.add("shadow-lg", "bg-yellow-600");
+        nb = true
+    } else {
+        if (xstatus != false) {
+            navbar.classList.remove("shadow-lg", "bg-yellow-600");
+            nb = false
+        }
+    }
+});
